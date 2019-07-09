c2b0e041c37401841112f916802e06d16696e2ed
Juan Pablo Barria Aguila <juan.barria@falp.org>
2019-07-09 12:30:59 -0400

    passing data with events

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -12,9 +12,12 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Test Recipe 2',' its a test 2 ','https://cdn.pixabay.com/photo/2015/10/26/07/21/soup-1006694_960_720.jpg')
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
   }
-
+  onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
+  }
 }
