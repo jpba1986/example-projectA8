52ad118dcac69261a87494e23ca4dee4d7c0a5ae
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-08-16 15:20:56 -0400

    singup request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+    idToken	    : string
+    email	    : string
+    refreshToken: string
+    expiresIn	: string
+    localId     : string
+}
+
+@Injectable({ providedIn:'root'})
+export class AuthService {
+  constructor(private http: HttpClient){}
+
+  signUp( email: string, password: string){
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCdKzuWEZ4fL_9HUlnoAMj7fLX6GyCrC1c',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        );
+    }
+}
