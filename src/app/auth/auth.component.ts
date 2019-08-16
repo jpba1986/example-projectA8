f8cd02a2a02d71fc0e3b1744337fac3250926ab9
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-08-16 15:34:28 -0400

    add spinner

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent{
     isLoginMode = true;
+    isLoading =false;
+    error: string =null;
 
     constructor(private authService : AuthService){}
 
@@ -25,15 +27,20 @@ export class AuthComponent{
         const email = form.value.email;
         const password = form.value.password;
 
+        this.isLoading = true;
+
         if(this.isLoginMode){
             // ....
         }else{
             this.authService.signUp(email,password).subscribe(
                 responseData =>{
                 console.log(responseData);
+                this.isLoading = false;
             },
             error =>{
                 console.log(error);
+                this.error = 'An error ocurred!';
+                this.isLoading = false;
             }
             );
         }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,4 +23,7 @@ export class AuthService {
             }
         );
     }
+
+
+
 }
