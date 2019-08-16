84561d05c8503ea591834ea58de092ad215ce68b
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-08-16 16:53:44 -0400

    auth state UI

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
 export interface AuthResponseData {
     idToken	    : string;
@@ -14,6 +15,8 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn:'root'})
 export class AuthService {
+user = new Subject<User>();
+
   constructor(private http: HttpClient){}
 
   signUp( email: string, password: string){
@@ -25,7 +28,10 @@ export class AuthService {
                 returnSecureToken: true
             }
         )
-        .pipe(catchError(this.handleError));
+        .pipe(catchError(this.handleError),
+        tap(resData =>{
+            this.handleAuthentication(resData.email, resData.localId,resData.idToken, +resData.expiresIn);
+        }));
     }
 
     login(email: string , password: string ){
@@ -37,7 +43,16 @@ export class AuthService {
                 returnSecureToken: true
             }
         )
-        .pipe(catchError(this.handleError));
+        .pipe(catchError(this.handleError),
+        tap(resData =>{
+            this.handleAuthentication(resData.email, resData.localId,resData.idToken, +resData.expiresIn);
+        }));
+    }
+
+    private handleAuthentication (email: string, userId: string, token: string, expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + +expiresIn * 1000);
+        const user = new User(email, userId, token, expirationDate);
+        this.user.next(user);
     }
 
     private handleError(errorRes: HttpErrorResponse){
