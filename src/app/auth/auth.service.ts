9e03c7bdb5d20f178e3e8b9abab8f28c4cbf6593
unknown <juanpablo.aguila@gmail.com>
2019-08-18 21:20:05 -0400

    logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, Subject, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
 
 export interface AuthResponseData {
     idToken	    : string;
@@ -17,7 +18,7 @@ export interface AuthResponseData {
 export class AuthService {
 user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private router: Router){}
 
   signUp( email: string, password: string){
         return this.http.post<AuthResponseData>(
@@ -47,6 +48,11 @@ user = new BehaviorSubject<User>(null);
         tap(resData =>{
             this.handleAuthentication(resData.email, resData.localId,resData.idToken, +resData.expiresIn);
         }));
+    }
+
+    logout(){
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleAuthentication (email: string, userId: string, token: string, expiresIn: number){
