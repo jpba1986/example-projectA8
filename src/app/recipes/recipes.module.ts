177f82c3643819b211c934566a2ae6f636384fc3
Juan Pablo DCS <juan.barria.falp@outlook.com>
2019-08-23 15:30:23 -0400

    shared module

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,10 +5,10 @@ import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -22,9 +22,10 @@ import { RecipesRoutingModule } from './recipes-routing.module';
   ] ,
   imports:[
     RouterModule, 
-    CommonModule, 
     ReactiveFormsModule, 
-    RecipesRoutingModule]
+    RecipesRoutingModule,
+    SharedModule
+  ]
 })
 export class RecipesModule{
 
